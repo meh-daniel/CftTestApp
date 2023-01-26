095d4223b530c3730128c5068699480de2efafea
daniel <meh.daniel@yandex.com>
2023-01-26 16:30:45 +0700

    добавление начальной разметки корневого constraint, и добавление во фрагменты начального кода с binding для продложения и развития этих экранов

--- app/src/main/java/test/task/cft/focus/start/com/presentation/screens/search/SearchFragment.kt
+++ app/src/main/java/test/task/cft/focus/start/com/presentation/screens/search/SearchFragment.kt
@@ -1,10 +1,34 @@
 package test.task.cft.focus.start.com.presentation.screens.search
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import test.task.cft.focus.start.com.R
+import test.task.cft.focus.start.com.databinding.FragmentSearchBinding
 
 class SearchFragment: Fragment(R.layout.fragment_search) {
 
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!!
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSearchBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
 }
--- app/src/main/java/test/task/cft/focus/start/com/presentation/screens/splash/SplashFragment.kt
+++ app/src/main/java/test/task/cft/focus/start/com/presentation/screens/splash/SplashFragment.kt
@@ -1,10 +1,34 @@
 package test.task.cft.focus.start.com.presentation.screens.splash
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import test.task.cft.focus.start.com.R
+import test.task.cft.focus.start.com.databinding.FragmentSplashBinding
 
 class SplashFragment: Fragment(R.layout.fragment_splash) {
 
+    private var _binding: FragmentSplashBinding? = null
+    private val binding get() = _binding!!
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSplashBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
 }
