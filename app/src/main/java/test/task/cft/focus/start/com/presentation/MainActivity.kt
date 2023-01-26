ac2cccac9560989c4ca0a642542d3f0a92857eec
daniel <meh.daniel@yandex.com>
2023-01-26 16:31:53 +0700

    уменьшил код в mainActivity

--- app/src/main/java/test/task/cft/focus/start/com/presentation/MainActivity.kt
+++ app/src/main/java/test/task/cft/focus/start/com/presentation/MainActivity.kt
@@ -1,12 +1,6 @@
 package test.task.cft.focus.start.com.presentation
 
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import test.task.cft.focus.start.com.R
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}
+class MainActivity : AppCompatActivity(R.layout.activity_main)
