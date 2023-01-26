1910eb5069af596c46e8edbdc279f88cefe488bf
daniel <meh.daniel@yandex.com>
2023-01-26 20:26:00 +0700

    добавление разметки

--- app/src/main/java/test/task/cft/focus/start/com/presentation/screens/splash/SplashFragment.kt
+++ app/src/main/java/test/task/cft/focus/start/com/presentation/screens/splash/SplashFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import test.task.cft.focus.start.com.R
 import test.task.cft.focus.start.com.databinding.FragmentSplashBinding
 
@@ -24,11 +25,25 @@ class SplashFragment: Fragment(R.layout.fragment_splash) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initSplashView()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun initSplashView() {
+        with(binding.content) {
+            alpha = 0f
+            animate().setDuration(2000).alpha(1f).withEndAction {
+                navigateToSearch()
+            }
+        }
+    }
+
+    private fun navigateToSearch() {
+        findNavController().navigate(R.id.action_splashFragment_to_searchFragment)
     }
 
 }
